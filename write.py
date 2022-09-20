7bb2aa86887353e6c2b46cb561a63af3314c9f9f
gon-xeneta <98804721+gon-xeneta@users.noreply.github.com>
2022-09-20 10:21:39 +0200

    Add more testing

--- write.py
+++ write.py
@@ -14,6 +14,7 @@ def write_file(filepath, additional_message=None):
     if additional_message is not None:
         additional_message += " "
     print(f"{additional_message}{elapsed}")
+    return elapsed
 
 ram_disk_filepath = "/tmp/ramdisk/file.txt"
 temp_path = mkdtemp()
@@ -21,7 +22,23 @@ disk_filepath = f"{temp_path}/file.txt"
 
 test_count = 10
 
-for i in range(test_count):
-    write_file(ram_disk_filepath, "Write to ramdisk     ")
-    write_file(disk_filepath, "Write to regular disk")
+ramdisk_score = 0
+disk_score = 0
 
+for i in range(test_count):
+    rmdsk = write_file(ram_disk_filepath, "Write to ramdisk     ")
+    dsk = write_file(disk_filepath, "Write to regular disk")
+
+    if rmdsk > dsk:
+        ramdisk_score += 1
+        print("Ramdisk point")
+    elif dsk > rmdsk:
+        disk_score += 1
+        print("Regular disk point")
+
+if ramdisk_score > disk_score:
+    print("Ramdisk is faster")
+elif disk_score > ramdisk_score:
+    print("Regular disk is faster")
+else:
+    print("Doesn't really matter")
